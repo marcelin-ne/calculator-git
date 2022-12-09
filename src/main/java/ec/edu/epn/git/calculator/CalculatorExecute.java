4f019d2203712fdf94931a4b6fc7a7534c128aeb
Marcelinne <73668598+marcelinnes@users.noreply.github.com>
2022-12-09 00:08:21 -0500

    Cambios en CalculatorExecute y cargar nuevo executable Jar de carpeta Target despues de usar los goals de Maven

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -3,15 +3,27 @@ package ec.edu.epn.git.calculator;
 public class CalculatorExecute{
 
     public static void main(String[] arg ){
+
+        System.out.println("---Practica N3 Automatizacion de Pruebas ");
+
         System.out.println("== Calculator Execute == ");
         Calculator c = new Calculator();
+        System.out.println("\n == Suma  == ");
         int addition=c.addition(4,7);
         System.out.println("c.addition(4,7)= "+ addition);
+        System.out.println("\n == Resta  == ");
         int substraction=c.subtraction(7,1);
         System.out.println("c.subtracction(7,1) =" + substraction);
+        System.out.println("\n == Division  == ");
+        double division=c.division(20,4);
+        System.out.println("c.division(20,4)= "+ division);
+        System.out.println("\n == Multiplicacion  == ");
+        double multiplicacion=c.division(100,2);
+        System.out.println("c.division(20,4)= "+ multiplicacion);
 
 
-        System.out.println("This is a Change ");
+
+
 
     }
 
