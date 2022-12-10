44c7f72df105ae0836c9dee096dc8a539ee5fb7e
Fernando Soto <fernando.soto@epn.edu.ec>
2022-12-10 16:14:31 -0500

    Primer commit

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -1,28 +1,25 @@
 package ec.edu.epn.git.calculator;
 
-public class CalculatorExecute{
+public class CalculatorExecute {
 
-    public static void main(String[] arg ){
+    public static void main(String[] arg) {
 
         System.out.println("---Practica N3 Automatizacion de Pruebas ");
 
         System.out.println("== Calculator Execute == ");
         Calculator c = new Calculator();
         System.out.println("\n == Suma  == ");
-        int addition=c.addition(4,7);
-        System.out.println("c.addition(4,7)= "+ addition);
+        int addition = c.addition(4, 7);
+        System.out.println("c.addition(4,7)= " + addition);
         System.out.println("\n == Resta  == ");
-        int substraction=c.subtraction(7,1);
-        System.out.println("c.subtracction(7,1) =" + substraction);
+        int subtraction = c.subtraction(7, 1);
+        System.out.println("c.subtracction(7,1) =" + subtraction);
         System.out.println("\n == Division  == ");
-        double division=c.division(20,4);
-        System.out.println("c.division(20,4)= "+ division);
+        double division = c.division(20, 4);
+        System.out.println("c.division(20,4)= " + division);
         System.out.println("\n == Multiplicacion  == ");
-        double multiplicacion=c.multiplicacion(100,2);
-        System.out.println("c.multiplicacion(100,2)= "+ multiplicacion);
-
-
-
+        double multiplication = c.multiplicacion(100, 2);
+        System.out.println("c.multiplication(100,2)= " + multiplication);
 
 
     }
--- src/main/java/ec/edu/epn/git/calculator/Main.java
+++ src/main/java/ec/edu/epn/git/calculator/Main.java
@@ -2,6 +2,6 @@ package ec.edu.epn.git.calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
